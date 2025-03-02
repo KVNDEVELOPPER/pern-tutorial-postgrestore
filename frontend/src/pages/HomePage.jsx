cd8217cbd2424d2639fac2b531a8686a5167db52
KVNDEVELOPPER <kevin.ntoya3@gmail.com>
2025-03-02 14:16:57 +0100

    Add product and modal screen created

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { useProductStore } from "../store/useProductStore";
-import { PlusCircleIcon, RefreshCwIcon } from "lucide-react";
+import { PackageIcon, PlusCircleIcon, RefreshCwIcon } from "lucide-react";
 import ProductCard from "./ProductCard";
+import AddProductModal from "../components/AddProductModal";
 
 const HomePage = () => {
   const { products, loading, error, fetchProducts } = useProductStore();
@@ -15,7 +16,12 @@ const HomePage = () => {
   return (
     <main className="container mx-auto px-4 py-8 max-w-6xl">
       <div className="flex justify-between items-center mb-8">
-        <button className="btn btn-primary">
+        <button
+          className="btn btn-primary"
+          onClick={() =>
+            document.getElementById("add_product_modal").showModal()
+          }
+        >
           <PlusCircleIcon className="size-5 mr-2" />
           Add Product
         </button>
@@ -23,7 +29,25 @@ const HomePage = () => {
           <RefreshCwIcon className="size-5" />
         </button>
       </div>
+
+      <AddProductModal />
+
       {error && <div className="alert alert-error mb-8">{error}</div>}
+
+      {products.length === 0 && !loading && (
+        <div className="flex flex-col justify-center items-center h-96 space-y-4">
+          <div className="bg-base-100 rounded-full p-6">
+            <PackageIcon className="size-12" />
+          </div>
+          <div className="text-center space-y-2">
+            <h3 className="text-2xl font-semibold">No products found</h3>
+            <p className="text-gray-500 max-w-sm">
+              Get started by adding your first product to the inventory
+            </p>
+          </div>
+        </div>
+      )}
+
       {loading ? (
         <div className="flex justify-center items-center h-64">
           <div className="loading loading-spinner loading-lg"></div>
